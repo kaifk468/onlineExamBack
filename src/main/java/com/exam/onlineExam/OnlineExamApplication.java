e4b17a725084eff5d8bde45f8db4609cc89d8ba3
kaifk468 <shivamjaiswalsdr@gmail.com>
2023-11-02 23:57:39 +0530

    added docker compose file which has three services like backend, frontend, postgres

--- src/main/java/com/exam/onlineExam/OnlineExamApplication.java
+++ src/main/java/com/exam/onlineExam/OnlineExamApplication.java
@@ -16,15 +16,6 @@ import com.exam.onlineExam.Services.UserService;
 public class OnlineExamApplication  implements CommandLineRunner{
 
 	@Autowired
-	UserService userService;
-
-	@Autowired
-	RoleRepo roleRepo;
-	
-	@Autowired
-	QuizeRepo quizeRepo;
-	
-	@Autowired
 	CategoryRepo categoryRepo;
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineExamApplication.class, args);
@@ -36,41 +27,6 @@ public class OnlineExamApplication  implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-        Quiz q=new Quiz();
-        q.setId(59);
-        //categoryRepo.deleteById(49L);
-        //quizeRepo.deletee(67L);;
-       // System.out.print("deleted");
-		//setting up user data;
-		// User user=new User();
-		// user.setEmail("kaifk468@gmail.com");
-		// user.setEnabled(true);
-		// user.setLastName("siiddique");
-		// user.setUserName("kaifk456468");
-		// user.setPhone("87878789809");
-		// user.setPassword("helloe moto how do uo do");
-
-		// // //creating a role for user
-		// Role role=new Role(11, "Normal");
-		// // //
-
-		// roleRepo.save(role);
-		// User_Role user_Role=new User_Role();
-		// user_Role.setRoleName(role);
-		// user_Role.setUser(user);
-
-		// // //adding userRoles for user
-		// Set<User_Role> userRoles=new HashSet<>();
-		// userRoles.add(user_Role);
-
-		// user.getUserRole().addAll(userRoles);
-		
-		//  userService.createUser(user,userRoles);//to seave thie to the datat base
-
-		// //user data with user role completed
-		
-
-		// System.out.println("commandLineRunner is  running");
 		
 	}
 
